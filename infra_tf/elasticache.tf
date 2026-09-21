c8d05466414d00292338489ba4a42d8b41390744
boycececil666gmailcom <boycececil666@gmail.com>
2026-09-21 17:35:27 +0900

    style: format terraform files with terraform fmt

--- infra_tf/elasticache.tf
+++ infra_tf/elasticache.tf
@@ -8,10 +8,10 @@ resource "aws_elasticache_subnet_group" "redis" {
 
 #region ElastiCache Redis
 resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "${var.app_name}-redis-${var.environment}"
-  engine               = "redis"
-  node_type            = "cache.t4g.micro"
-  num_cache_nodes      = 1
+  cluster_id               = "${var.app_name}-redis-${var.environment}"
+  engine                   = "redis"
+  node_type                = "cache.t4g.micro"
+  num_cache_nodes          = 1
   parameter_group_name     = "default.redis7"
   port                     = 6379
   subnet_group_name        = aws_elasticache_subnet_group.redis.name
